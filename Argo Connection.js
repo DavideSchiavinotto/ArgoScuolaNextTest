c9180138b85310377aa9c0a3cd32a9fa161d584d
DavideSchiavinotto <44573807+DavideSchiavinotto@users.noreply.github.com>
2019-01-15 23:09:37 +0100

    Create Argo Connection.js

---
+++ Argo Connection.js
@@ -0,0 +1,15 @@
+const ArgoAPI = require('argoapi')
+
+ArgoAPI.login('school code','username','password')
+
+    .then(message => {
+
+    //successfull message
+
+    })
+
+    .catch(err => {
+
+        //error message
+
+    })
